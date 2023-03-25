a8c917c858e8342b9171b314f712c284e71578d9
Florian Schwanz <florian.schwanz@interoberlin.de>
2023-03-25 21:03:30 +0100

    chore: add data loader #1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 from lib.extract.data_extractor import extract_data
+from lib.load.data_loader import load_data
 from lib.tracking_decorator import TrackingDecorator
 from lib.transform.data_blender import blend_data
 from lib.transform.data_copier import copy_data
@@ -39,6 +40,7 @@ def main(argv):
     manifest_path = os.path.join(script_path, "data-product.yml")
     raw_path = os.path.join(script_path, "raw")
     workspace_path = os.path.join(script_path, "workspace")
+    data_path = os.path.join(script_path, "data")
 
     #
     # Extract
@@ -53,6 +55,12 @@ def main(argv):
     copy_data(source_path=raw_path, results_path=workspace_path, clean=clean, quiet=quiet)
     convert_data_to_csv(source_path=workspace_path, results_path=workspace_path, clean=clean, quiet=quiet)
     blend_data(source_path=workspace_path, results_path=workspace_path, clean=clean, quiet=quiet)
+
+    #
+    # Transform
+    #
+
+    load_data(source_path=workspace_path, results_path=data_path, clean=clean, quiet=quiet)
 
 
 if __name__ == "__main__":
